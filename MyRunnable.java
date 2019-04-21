bf3411934e79574941a02b93d54666f488bad964
Robert Mora <39216449+MoraRobert@users.noreply.github.com>
2019-04-21 23:24:24 +0200

    Add files via upload

---
+++ MyRunnable.java
@@ -0,0 +1,11 @@
+package com.Robert;
+
+import static com.Robert.ThreadColor.ANSI_RED;
+
+public class MyRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println(ANSI_RED + "Hello from MyRunnable's implementation of run");
+    }
+}
